9406f0edb7d6d8fcc29f28fc7cbbf4537428cac1
An Ngo <a.ngo20@students.bchigh.edu>
2017-07-19 17:55:15 +0000

    start

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
-    $('add-new-todo-button').click(function(){
-        var todoBlock=$(this).closest('#new-todo-block');
-        var description=todoBlock.find('#new-todo-description');
-        var pomodoroEstimate=todoBlock.find('#new-pomodoro-estimate');
-    $("ul").append("<li><inputtype='checkbox'/>"+description.val()+"<span class="pomodoro-estimate">"+
-    pomodoroEstimate.val()+" pomodoros</span></li>")
-    )
-    })
+    $("#add-new-todo-button").click(function(){
+        var todoBlock= $(this).closest("#new-todo-block");
+        var description = todoBlock.find("#new-todo-description");
+        var pomodoroEstimate = todoBlock.find("#new-pomodoro-estimate");
+        
+    $("ul").append("<li><input type = 'checkbox'/>" + description.val() + "<span class = 'pomodoro-estimate'>" + 
+    pomodoroEstimate.val() + " pomodoros</span></li>");
     
-})
+    description.val("");
+    pomodoroEstimate.val("");
+});
+});
